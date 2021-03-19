d53c513e00fa4df5ab827f73b0cd2ba3b8843099
Jonathan Dawson <dawsonjed@gmail.com>
2021-03-19 12:57:46 -0500

    ran prettier, but i thought it would run each time i commit

--- ff-backend/lambda/getFood/getFood.ts
+++ ff-backend/lambda/getFood/getFood.ts
@@ -2,25 +2,25 @@ import { Client } from 'pg';
 import createresponse from 'createresponse';
 
 export async function handler() {
-	const result = await getFood().then((res) => res);
-	return createresponse(200, JSON.stringify(result));
+  const result = await getFood().then((res) => res);
+  return createresponse(200, JSON.stringify(result));
 }
 
 async function getFood() {
-	const client = new Client({
-		host: process.env.PGHOST,
-		user: process.env.PGUSER,
-		password: process.env.PGPASSWORD,
-		database: process.env.PGDATABASE,
-	});
+  const client = new Client({
+    host: process.env.PGHOST,
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABASE,
+  });
 
-	let q = 'select * from food';
+  let q = 'select * from food';
 
-	await client.connect();
+  await client.connect();
 
-	const result = await client.query(q);
+  const result = await client.query(q);
 
-	client.end();
+  client.end();
 
-	return result.rows;
+  return result.rows;
 }
